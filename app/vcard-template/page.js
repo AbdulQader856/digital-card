deff6be5453e6c46ec483459e2f78ca97b41739c
Abdul Qader Kagzi <abdulqaderkagzi@gmail.com>
2025-06-04 14:52:47 +0530

    Created vCard 17

--- app/vcard-template/page.js
+++ app/vcard-template/page.js
@@ -1,11 +1,9 @@
 import Vcard from '../../src/components/template/Vcard/Vcard';
-// import Vcard3 from '../../src/components/template/Vcard3/Vcard3';
 
 export default function VcardTemplatePage() {
   return (
     <div className="container">
       <Vcard /> 
-      {/* <Vcard3 /> */}
     </div>
   );
 }
